0c17d4a643deb81d7ece922a61e8d544859b5bc2
Nigel Andahua <nigel2421@gmail.com>
2026-02-26 10:51:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4856100105-bcae2",
+  "appId": "1:930967597954:web:a3e70d97f6cc1c7744959b",
+  "apiKey": "AIzaSyBl--VHcX8oxNJeA77Prg8NvTw_yEL3tW0",
+  "authDomain": "studio-4856100105-bcae2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "930967597954"
+};
